4d09168e70f6099ddc61b2f1afeb8d6bfda98de1
iteny <iteny@msn.com>
2016-06-11 20:32:35 +0800

    golang基础结构

--- golang-base.go
+++ golang-base.go
@@ -18,4 +18,6 @@ type golang interface{}
 func main() {
 	fmt.Println("Hello World!你好，世界！")
 }
+//Go语言可见性规则，使用大小写来决定该常量，变量，类型，接口，结构或函数是否可以被外部包所调用
+//根据约定，函数名首字幕小写即为private(私有的),函数名首字母大写即为public(公共的)
 
